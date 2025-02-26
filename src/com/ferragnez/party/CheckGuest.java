c1374a68bea57bd2941fb77b7d16da8a560e4096
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-02-26 17:24:12 +0100

    inizializzato l'array degli invitati

---
+++ src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,12 @@
+package com.ferragnez.party;
+
+public class CheckGuest {
+    public static void main(String[] args) {
+        System.out.println("");
+
+        String[] Invitati = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+        for (int i = 0; i < Invitati.length; i++) {
+            System.out.println(Invitati[i]);
+        }
+    }
+}
